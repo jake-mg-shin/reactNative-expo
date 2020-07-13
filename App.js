3bcd0e03a647445932e23bd8b2c71b411935beed
jake-mg-shin <60331338+jake-mg-shin@users.noreply.github.com>
2020-07-13 08:28:46 -0600

    add modal and refactoring

--- App.js
+++ App.js
@@ -14,23 +14,41 @@ import GoalInput from './components/GoalInput';
 
 export default function App() {
     const [courseGoals, setCourseGoals] = useState([]);
+    const [isAddMode, setIsAddMode] = useState(false);
 
     const addGoalHandler = (goalTitle) => {
         setCourseGoals((currentGoals) => [
             ...currentGoals,
             { id: Math.random().toString(), value: goalTitle },
         ]);
+        setIsAddMode(false);
+    };
+
+    const removeGoalHandler = (goalId) => {
+        setCourseGoals((currentGoals) => {
+            return currentGoals.filter((goal) => goal.id !== goalId);
+        });
+    };
+
+    const cancelGoalAdditionalHandler = () => {
+        setIsAddMode(false);
     };
 
     return (
         <View style={styles.screen}>
-            <GoalInput onAddGoal={addGoalHandler} />
+            <Button title='Add New Goal' onPress={() => setIsAddMode(true)} />
+            <GoalInput
+                visible={isAddMode}
+                onAddGoal={addGoalHandler}
+                onCancel={cancelGoalAdditionalHandler}
+            />
             <FlatList
                 keyExtractor={(item, index) => item.id}
                 data={courseGoals}
                 renderItem={(itemData) => (
                     <GoalItem
-                        onPress={() => console.log('Does that work?')}
+                        id={itemData.item.id}
+                        onDelete={removeGoalHandler}
                         title={itemData.item.value}
                     />
                 )}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
 const GoalInput = (props) => {
     const [enteredGoal, setEnteredGoal] = useState('');
@@ -8,19 +8,34 @@ const GoalInput = (props) => {
         setEnteredGoal(enteredText);
     };
 
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+    };
+
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder='Course Goal'
-                style={styles.input}
-                onChangeText={goalInputHandler}
-                value={enteredGoal}
-            />
-            <Button
-                title='ADD'
-                onPress={props.onAddGoal.bind(this, enteredGoal)}
-            />
-        </View>
+        <Modal visible={props.visible} animationType='slide'>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Course Goal'
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title='ADD' onPress={addGoalHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button
+                            title='CANCEL'
+                            color='red'
+                            onPress={props.onCancel}
+                        />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 };
 
@@ -28,8 +43,8 @@ export default GoalInput;
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center',
     },
     input: {
@@ -37,5 +52,14 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 1,
         padding: 10,
+        marginBottom: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '40%',
+    },
+    button: {
+        width: '60%',
     },
 });
